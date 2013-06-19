418d59e01ba8684c9b8ab762f6697d6c26a5065c
edrezen <edrezen@irisa.fr>
2013-06-19 15:28:38 +0200

    Reactivate Minia part

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main (int argc, char* argv[])
 
         tool.add (new BankConverter ());
         tool.add (new DSK           ());
-//        tool.add (new Minia         ());
+        tool.add (new Minia         ());
 
         tool.run (argc, argv);
     }
