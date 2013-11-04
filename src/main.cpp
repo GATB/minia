6e507519ca42ae051d9d44483ab2d01596a85dff
edrezen <edrezen@irisa.fr>
2013-11-04 10:02:00 +0100

    cosmit

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,34 @@
 
 /********************************************************************************/
 
-#include <gatb/gatb_core.hpp>
+#include <Minia.hpp>
+
+using namespace std;
 
 /********************************************************************************/
 
 int main (int argc, char* argv[])
 {
+    // We define a try/catch block in case some method fails (bad filename for instance)
+    try
+    {
+        /** We execute dsk. */
+        Minia().run (argc, argv);
+    }
+
+    catch (OptionFailure& e)
+    {
+        e.getParser().displayErrors (stdout);
+        e.getParser().displayHelp   (stdout);
+        return EXIT_FAILURE;
+    }
+
+    catch (Exception& e)
+    {
+        cerr << "EXCEPTION: " << e.getMessage() << endl;
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
+
