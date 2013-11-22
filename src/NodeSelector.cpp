75998a95292747615670b01d06a6514f85c23515
edrezen <edrezen@irisa.fr>
2013-11-22 11:36:18 +0100

    Working version

--- src/NodeSelector.cpp
+++ src/NodeSelector.cpp
@@ -11,9 +11,7 @@
 
 #include <NodeSelector.hpp>
 #include <Frontline.hpp>
-
-#undef NDEBUG
-#include <cassert>
+#include <Traversal.hpp>
 
 using namespace std;
 
@@ -145,15 +143,18 @@ bool NodeSelectorBest::select (const Node& source, Node& result)
 {
     int sum_depths = 0;
 
-int max_depth   = 500;
-int max_breadth = 20;
-
     /** First selection. */
     if (_firstSelector.select (source, result) == false)  { return false; }
+
+    /** We use a monument traversal. */
+    MonumentTraversal monument (_graph, NullTerminator::singleton());
 
     /** We try each direction (outcoming and incoming). */
     foreach_direction (dir)
     {
+        Node previousNode;
+        bool hasPreviousNode = false;
+
         // do a BFS to make sure we're not inside a bubble or tip
         Frontline frontline (dir, _graph, _terminator, result);
 
@@ -163,21 +164,17 @@ int max_breadth = 20;
             if (!should_continue)  {  break;  }
 
             // put the same contraints as in a bubble
-            if (frontline.depth() > max_depth || frontline.size() > max_breadth)  {  break;  }
+            if (frontline.depth() > monument.getMaxDepth() || frontline.size() > monument.getMaxBreadth())  {  break;  }
 
             // stopping condition: nothing more to explore
             if (frontline.size() == 0)  {  break;  }
 
-            char useless_string[max_depth+1];
-            int useless_int;
-
-#if 0
             if (frontline.size() <= 1)
             {
                 Node currentNode;
                 if (frontline.size() == 1)  {  currentNode = frontline.front().node;  }
 
-                if ((previous_kmer != 0) && terminator->is_branching(previous_kmer))
+                if (hasPreviousNode && _terminator.is_branching(previousNode))
                 {
                     /* the current situation is:
                      *
@@ -194,34 +191,28 @@ int max_breadth = 20;
                      *   so, by looking one k-mer ahead, we make sure that previous_kmer only branches to the right
                      *
                      */
-                    set<kmer_type> all_involved_extensions;
-                    Terminator *save_terminator = terminator;
-                    terminator = NULL; // do not use terminator in the following bubble traversal
-                    if (explore_branching(previous_kmer, 1-previous_strand, (char*)useless_string, useless_int, current_kmer, &all_involved_extensions))
+                    set<Node> all_involved_extensions;
+                    PATH consensus;
+
+                    if (monument.explore_branching (previousNode, reverse(dir), consensus, currentNode, all_involved_extensions))
                     {
-                        if (debug) printf("depth %d useless int %d and starting belongs %d nb involved nodes %d\n",frontline.depth,useless_int,all_involved_extensions.find(starting_kmer) != all_involved_extensions.end(),all_involved_extensions.size());
-                        if (all_involved_extensions.find(starting_kmer) != all_involved_extensions.end())
+                        if (all_involved_extensions.find(result) != all_involved_extensions.end())
                         {
-                            terminator = save_terminator;
                             return false; // starting_kmer is in a tip/bubble starting from current_kmer
                         }
-
                     }
-                    terminator = save_terminator;
                 }
             }
-#endif
 
             // update previous_kmer
             if (frontline.size() == 1)
             {
-                Node currentNode = frontline.front().node;
+                previousNode    = frontline.front().node;
+                hasPreviousNode = true;
             }
             else
             {
-#if 0
-                previous_kmer = 0;
-#endif
+                hasPreviousNode = false;
             }
         }
         while (1);
