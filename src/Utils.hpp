e13349441f6c34027480b0b3bd004d3f96ba0fbc
edrezen <edrezen@irisa.fr>
2014-01-14 12:41:16 +0100

    Modify headers of *.hpp and *.cpp => Affero GPL

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -1,9 +1,20 @@
 /*****************************************************************************
- *   GATB : Genome Assembly Tool Box                                         *
- *   Authors: [R.Chikhi, G.Rizk, E.Drezen]                                   *
- *   Based on Minia, Authors: [R.Chikhi, G.Rizk], CeCILL license             *
- *   Copyright (c) INRIA, CeCILL license, 2013                               *
- *****************************************************************************/
+ *   GATB : Genome Assembly Tool Box
+ *   Copyright (C) 2014  R.Chikhi, G.Rizk, E.Drezen
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*****************************************************************************/
 
 #ifndef _GATB_TOOLS_UTILS_HPP_
 #define _GATB_TOOLS_UTILS_HPP_
