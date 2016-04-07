18bd7127d5f151dc5cc92d9e0dbaa4950a2fd434
Patrick Durand <patrick.durand@inria.fr>
2016-04-07 08:59:13 +0200

    fix path locator of minia binary

--- test/test_ERR039477.sh
+++ test/test_ERR039477.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
+# look for minia binary. In devel mode, it's in ../build/bin directory.
+# In production mode, it's in ../bin directory.
+if [ -f "../bin/minia" ]
+then
+ bindir="../bin"
+elif [ -f "../build/bin/minia" ]
+then
+ bindir="../build/bin"
+else
+ echo "could not find a compiled Minia binary"
+ exit 1
+fi
+
 ################################################################################
 # we download some banks
 ################################################################################
@@ -9,7 +22,7 @@ wget "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR039/ERR039477/ERR039477.fastq.gz"
 # we launch minia; note that we use only one thread (no real time issues with
 # potential different results)
 ################################################################################
-../build/minia -nb-cores 1 -in ERR039477.fastq.gz
+$bindir/minia -nb-cores 1 -in ERR039477.fastq.gz
 
 ################################################################################
 # we check the result
