151c95b8dfea524672cf25e03be91491b062eb8f
rchikhi <rayan.chikhi@univ-lille1.fr>
2017-04-10 00:51:18 +0200

    updated to new gatb-core

--- scripts/strip_bcalm_links.py
+++ scripts/strip_bcalm_links.py
@@ -3,11 +3,11 @@ file=sys.argv[1]
 for line in open(file):
      line=line.strip()
      if not line.startswith(">"):
-        print line.strip()
+        print(line.strip())
         continue
      if "L:" in line:
          s = line.split("L:")
      else:
          s = [line.strip()]
-     print s[0]
+     print(s[0])
 
