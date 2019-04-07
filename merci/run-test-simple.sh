7a5db91e2df8cb2cf2feeeab4f597f56ca315ec7
rchikhi <rayan.chikhi@univ-lille1.fr>
2019-04-07 21:28:02 +0200

    update gatb-core

--- merci/run-test-simple.sh
+++ merci/run-test-simple.sh
@@ -1,3 +1,8 @@
 cd test-simple; 
 ../../buildk32/merci -kmer-size 11 -reads reads.fa -assembly assembly.fa -verbose  1 ;
 cd ..
+
+echo "wc, should have 4 lines:"
+wc -l test-simple/assembly.fa
+echo "wc, should have 2 lines:"
+wc -l test-simple/assembly.fa.merci
