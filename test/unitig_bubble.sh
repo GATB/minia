71484e8e7d49d398076168ec1cfdd3f7e053f4a7
Rayan  CHIKHI <rayan.chikhi@pasteur.fr>
2023-11-05 21:19:41 +0000

    tweaks to simplifications

---
+++ test/unitig_bubble.sh
@@ -0,0 +1,3 @@
+# plot twist, that bubble can't be crushed with default params, it's too long!
+# so that test case isn't so interesting..
+~/minia/build/bin/minia -in unitig_bubble -skip-bcalm -skip-bglue -redo-links 
